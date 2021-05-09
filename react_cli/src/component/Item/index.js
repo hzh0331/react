48016a8a5a46eee69ce1bd2418b53131c2b910b7
ZihaoHan <972802149@qq.com>
2021-05-08 22:34:11 -0700

    item color change

--- react_cli/src/component/Item/index.js
+++ react_cli/src/component/Item/index.js
@@ -2,15 +2,24 @@ import React, {Component} from 'react';
 import "./index.css"
 
 class Item extends Component {
+    state = {mouse:false}
+
+    handleMouse(flag){
+        return ()=>{
+            this.setState({ mouse: flag})
+        }
+    }
+
     render() {
+        let {mouse} = this.state
         let {id, name, done} = this.props
         return (
-            <li>
+            <li style={{backgroundColor : mouse ? '#ddd' : 'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
                 <label>
                     <input type="checkbox" defaultChecked={done}/>
                     <span>{name}</span>
                 </label>
-                <button className="btn btn-danger" style={{display:'none'}}>删除</button>
+                <button className="btn btn-danger" style={{display: mouse ? 'block' : 'none'}}>删除</button>
             </li>
         );
     }
