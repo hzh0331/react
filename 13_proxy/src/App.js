45f7baf8b0f86c1067193cc7f8785fc00fcd0183
ZihaoHan <972802149@qq.com>
2021-05-10 16:46:30 -0700

    proxy

---
+++ 13_proxy/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{Component} from "react";
+import axios from "axios";
+
+export default class App extends Component {
+  getStudentInfo = () =>{
+      axios.get("http://localhost:3000/students").then(
+          response=>{
+              console.log(response.data)
+          },fail=>{
+
+          }
+      )
+  }
+  render() {
+    return (
+        <div>
+          <button onClick={this.getStudentInfo}>click for students' information</button>
+        </div>
+    )
+  }
+
+
+}
